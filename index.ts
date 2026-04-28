e92b77831c935b7004ac03f22e8bb067b67c891d
Makintosh101 <88837479+Makintosh101@users.noreply.github.com>
2026-04-28 23:14:32 +0200

    Delete index.ts

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./crew-avatar";
-export * from "./crew-display-name";
-export * from "./crew-required-fields";
-export * from "./dial-codes";
-export type { Database, Json } from "./supabase-types";
